d65fdebb95c7f1386ed5746dac248ffbeb21666e
Roximia <emilka@tech-net.pl>
2018-02-03 19:52:00 +0100

    kodzik w projekcie visuala

---
+++ odejmowanie vs/odejmowanie vs/sors.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "odejmowanie.h"
+
+
+using namespace std;
+
+int main(){
+	int a, b;
+	cin >> a >> b;
+	cout << endl << odejmowanie(a, b);
+	system("pause");
+	return 0;
+}
