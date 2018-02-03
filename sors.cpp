5df72fe15f3dd6f5c76df9d36907363a9f997d46
Morstrus <adam.pianowski@gmail.com>
2018-02-03 17:35:22 +0100

    sors bardziej cywilizowany

--- sors.cpp
+++ sors.cpp
@@ -5,7 +5,16 @@
 using namespace std;
 
 int main(){
-	int a, b;
+	int a, b, z;
 	cin >> a >> b;
-	cout << endl << dodawanie(a, b);
+	cout << "Co chcesz zrobic?" << endl;
+	cout << "1. Dodac" << endl;
+	cin >> z;
+	switch(z)
+	{
+		case 1:
+		cout << dodawanie(a, b);
+		break;
+	}
+	
 }
