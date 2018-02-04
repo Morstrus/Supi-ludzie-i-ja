d9a95b1a0434c42e5d9f0a45225273ffee68bae5
Meanness104 <35897553+Meanness104@users.noreply.github.com>
2018-02-04 02:57:45 +0100

    Update sors.cpp

--- sors.cpp
+++ sors.cpp
@@ -12,16 +12,21 @@ int main(){
 	cout << "Co chcesz zrobic?" << endl;
 	cout << "1. Dodac" << endl;
 	cout << "2. Odejmowanie" << endl;
-	cout << "3. Dzielenie" << endl;
+	cout << "3. Mnozenie"<< endl;
+	cout << "4. Dzielenie" << endl;
 	cin >> z;
 	switch(z)
 	{
 		case 1:
 		cout << dodawanie(a, b);
+		break;
 		case 2:
 		cout << odejmowanie(a, b);
 		break;
 		case 3:
+		cout << mnozenie(a, b);
+		break;
+		case 4:
 		cout << dzielenie(a,b);
 		break;
 	}
